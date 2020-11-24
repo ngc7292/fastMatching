33e643cc1d781cb456bb220b72654cfb8f9e318d
ngc7293 <feizhaoye@gmail.com>
2020-11-24 22:02:11 +0800

    feat: 合并esim与bert并统一格式

--- args_test.py
+++ args_test.py
@@ -4,3 +4,25 @@ __title__="args_test"
 __author__="ngc7293"
 __mtime__="2020/11/24"
 """
+import argparse
+
+# define hyper-parameters
+argument = argparse.ArgumentParser()
+argument.add_argument('--embedding', choices=['glove', 'word2vec'], default='glove')
+argument.add_argument('--batch-size-per-gpu', type=int, default=256)
+argument.add_argument('--n-epochs', type=int, default=200)
+argument.add_argument('--lr', type=float, default=1e-5)
+argument.add_argument('--save-dir', type=str, default=None)
+argument.add_argument('--cntn-depth', type=int, default=1)
+argument.add_argument('--cntn-ns', type=int, default=200)
+argument.add_argument('--cntn-k-top', type=int, default=10)
+argument.add_argument('--cntn-r', type=int, default=5)
+argument.add_argument('--dataset', choices=['qnli', 'rte', 'snli', 'mnli'], default='qnli')
+argument.add_argument('--device', type=list, default=[1,2])
+
+arg = vars(argument.parse_args())
+
+arg['train'] = 1
+
+
+print(arg.device)
