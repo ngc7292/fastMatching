6f5230c1d9510822c1f03d767d037727836b6ad7
ngc7293 <feizhaoye@gmail.com>
2020-11-24 22:01:00 +0800

    feat: 合并esim与bert并统一格式

---
+++ BERT/matching_bert.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+__title__="matching_bert"
+__author__="ngc7293"
+__mtime__="2020/11/24"
+"""
---
+++ ESIM/matching_esim.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+__title__="matching_esim"
+__author__="ngc7293"
+__mtime__="2020/11/23"
+"""
