4193d6a2140a8593933fbf4ecb77a1b4029c2f5f
ngc7293 <feizhaoye@gmail.com>
2021-01-14 19:31:43 +0800

    add dc-bert model

--- args_test.py
+++ args_test.py
@@ -7,22 +7,26 @@ __mtime__="2020/11/24"
 import argparse
 
 # define hyper-parameters
-argument = argparse.ArgumentParser()
-argument.add_argument('--embedding', choices=['glove', 'word2vec'], default='glove')
-argument.add_argument('--batch-size-per-gpu', type=int, default=256)
-argument.add_argument('--n-epochs', type=int, default=200)
-argument.add_argument('--lr', type=float, default=1e-5)
-argument.add_argument('--save-dir', type=str, default=None)
-argument.add_argument('--cntn-depth', type=int, default=1)
-argument.add_argument('--cntn-ns', type=int, default=200)
-argument.add_argument('--cntn-k-top', type=int, default=10)
-argument.add_argument('--cntn-r', type=int, default=5)
-argument.add_argument('--dataset', choices=['qnli', 'rte', 'snli', 'mnli'], default='qnli')
-argument.add_argument('--device', type=list, default=[1,2])
+# argument = argparse.ArgumentParser()
+# argument.add_argument('--embedding', choices=['glove', 'word2vec'], default='glove')
+# argument.add_argument('--batch-size-per-gpu', type=int, default=256)
+# argument.add_argument('--n-epochs', type=int, default=200)
+# argument.add_argument('--lr', type=float, default=1e-5)
+# argument.add_argument('--save-dir', type=str, default=None)
+# argument.add_argument('--cntn-depth', type=int, default=1)
+# argument.add_argument('--cntn-ns', type=int, default=200)
+# argument.add_argument('--cntn-k-top', type=int, default=10)
+# argument.add_argument('--cntn-r', type=int, default=5)
+# argument.add_argument('--dataset', choices=['qnli', 'rte', 'snli', 'mnli'], default='qnli')
+# argument.add_argument('--device', type=list, default=[1,2])
+#
+# arg = vars(argument.parse_args())
+#
+# arg['train'] = 1
 
-arg = vars(argument.parse_args())
+import torch
+from torch.nn import Transformer
 
-arg['train'] = 1
+a = Transformer(nhead=8, dropout=0.2, d_model=12, num_encoder_layers=2)
 
 
-print(arg)
--- model/dcbert.py
+++ model/dcbert.py
@@ -1,6 +1,111 @@
 # -*- coding: utf-8 -*-
-"""
-__title__="dcbert"
-__author__="ngc7293"
-__mtime__="2021/1/14"
-"""
+import torch
+from torch import nn
+
+from fastNLP.embeddings import BertEmbedding
+from torch.nn import Transformer
+from fastNLP.core.utils import seq_len_to_mask
+
+
+class MyDropout(nn.Module):
+    def __init__(self, p):
+        super().__init__()
+        assert 0 <= p <= 1
+        self.p = p
+
+    def forward(self, x):
+        if self.training and self.p > 0.0001:
+            # print('mydropout!')
+            mask = torch.rand(x.size())
+            # print(mask.device)
+            mask = mask.to(x)
+            # print(mask.device)
+            mask = mask.lt(self.p)
+            x = x.masked_fill(mask, 0) / (1 - self.p)
+        return x
+
+
+class BERT_Matching(nn.Module):
+    def __init__(self, dataBundle, args):
+        super().__init__()
+        self.vocabs = dataBundle.vocabs
+        self.ptm_encoder = BertEmbedding(
+            self.vocabs['words1'],
+            model_dir_or_name=args.model_dir_or_name,
+            pool_method=args.pool_method,
+            include_cls_sep=True,
+            auto_truncate=True
+        )
+
+        self.bert_dropout = MyDropout(args.bert_dropout)
+
+        self.loss_func = nn.CrossEntropyLoss()
+        self.linear_after_ptm = nn.Linear(self.ptm_encoder.embed_size, args.transformer_dim)
+        self.transformer = Transformer(
+            num_encoder_layers=args.transformer_layer,
+            num_decoder_layers=args.transformer_layer,
+            dim_feedforward=self.ptm_encoder.embed_size,
+            d_model=args.transformer_dim,
+            nhead=args.transformer_num_head,
+            dropout=args.transformer_dropout
+        )
+
+        self.decoder_input_dim = args.transformer_dim * 2
+        if args.decoder_mode == "linnear":
+            self.ffn = nn.Linear(self.decoder_input_dim, len(self.vocabs['target']))
+        elif args.decoder_mode == "multi":
+            self.ffn = nn.Sequential(nn.Linear(self.decoder_input_dim, self.decoder_input_dim * 2),
+                                     nn.LeakyReLU(), MyDropout(args.decode_dropout),
+                                     nn.Linear(self.decoder_input_dim * 2, len(self.vocabs['target'])))
+        else:
+            raise NotImplementedError
+
+    def forward(self, **kwargs):
+        seq_len1 = kwargs['seq_len1']
+        seq_len2 = kwargs['seq_len2']
+        target = kwargs['target']
+
+        words1 = kwargs['words1']
+        words2 = kwargs['words2']
+        batch_size = words1.size(0)
+
+        # pre train encoder
+        encoded_1 = self.encode_words(words1)
+        encoded_2 = self.encode_words(words2)
+
+        # decoder
+        pred = self.transformer_and_decoder(encoded_1, encoded_2, batch_size, seq_len1, seq_len2)
+
+        loss = self.loss_func(pred, target)
+        result = {'pred': pred, 'loss': loss}
+
+        return result
+
+    def encode_words(self, words):
+        return self.bert_dropout(self.ptm_encoder(words))
+
+    def transformer_and_decoder(self, encoded_1, encoded_2, batch_size, seq_len1, seq_len2):
+        seq_len1 += 2  # add [cls], [seq]
+        seq_len2 += 2  # add [cls], [seq]
+
+        encoded_cat = self.linear_after_ptm(torch.cat([encoded_1, encoded_2], dim=1))
+
+        encoded_1_mask = seq_len_to_mask(seq_len1, max_len=encoded_1.size(1))
+        encoded_2_mask = seq_len_to_mask(seq_len2, max_len=encoded_2.size(1))
+
+        encoded_concat_mask = torch.cat([encoded_1_mask, encoded_2_mask], dim=1)
+
+        transformer_out = self.transformer(encoded_cat, encoded_concat_mask)
+
+        # 取出 transformer cls 拼在一起过decoder
+        cls_1_2 = transformer_out[:, [0, encoded_1.size(1)]]
+        cls_1_2 = cls_1_2.view(batch_size, -1)
+
+        pred = self.decoder(cls_1_2)
+        return pred
+
+    def get_encoder_token(self, words):
+        return self.encode_words(words)
+
+    def predict(self):
+        pass
